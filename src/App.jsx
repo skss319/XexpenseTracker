3bf57e13dc51733a5a24eb9a2ea9899395556c66
skss319 <ss319saurav@gmail.com>
2025-07-21 17:22:05 +0530

    latest changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home/Home";
+import { SnackbarProvider } from "notistack";
+
+function App() {
+  return (
+    <SnackbarProvider>
+      <Home />
+    </SnackbarProvider>
+  );
+}
+
+export default App;
